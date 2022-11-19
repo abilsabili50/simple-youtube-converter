b879f0771025964f68e707e5b21bb9786a8b55cf
Abilsabili <abilsabili50@gmail.com>
2022-11-19 16:34:59 +0700

    add fail response

--- script.js
+++ script.js
@@ -17,12 +17,16 @@ btnConvert.addEventListener("click", () => {
 		}),
 	})
 		.then((response) => response.json())
-		.then((response) => {
-			spinner.style.display = "none";
-			download.innerHTML = `<a href="${response.data.link}" target="_blank" class="btn btn-primary" type="button">Download</a>`;
-		})
 		.catch((error) => {
 			spinner.style.display = "none";
 			download.innerHTML = `<p style="color: red">${error.message}</p>`;
+		})
+		.then((response) => {
+			if (response.status == "fail") {
+				spinner.style.display = "none";
+				download.innerHTML = `<p style="color: red">${response.message}</p>`;
+			}
+			spinner.style.display = "none";
+			download.innerHTML = `<a href="${response.data.link}" target="_blank" class="btn btn-primary" type="button">Download</a>`;
 		});
 });
